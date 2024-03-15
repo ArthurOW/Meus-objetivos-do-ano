e8dd2125cf04c585ccf69643e10015a63c593241
ArthurOW <126161571+ArthurOW@users.noreply.github.com>
2024-03-15 11:12:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 const botoes = querySelectorAll(".botao");
-console.log(botoes);
+for(let i= 0; i< botoes.lenght; i++){
+    botoes[i].onclick = function(){
+        for(j=0; j<botoes.lenght; j++){
+            botoes[j].claslist.remove("ativo");
+        }
+        botoes[i].classlist.add("ativo");
+    }
+}
