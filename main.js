c51acb63ba554a3006c7990a7d6a6a377d8dc46c
ArthurOW <126161571+ArthurOW@users.noreply.github.com>
2024-04-19 10:28:04 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo01 = new Date("2024-11-30T00:00:00");
 let tempoAtual = new Date();
-contadores[0].textContent = tempoObjetivo01 - tempoAtual;
+contadores[0].textContent = calculaTempo(tempoObjetivo01);
 for(let i= 0; i< botoes.length; i++){
     botoes[i].onclick = function(){
         for(j=0; j<botoes.length; j++){
@@ -13,4 +13,16 @@ for(let i= 0; i< botoes.length; i++){
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
+}
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date( );
+    let tempoFinal = tempoObjetivo - tempoAtual; 
+    let segundos = Math.floor (tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+    segundos %=60;
+    minutos %=60;
+    horas %=24;
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos "; 
 }
