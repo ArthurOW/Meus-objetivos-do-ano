cbb6e8afea507434fafccaa3531c5e46beac6b95
ArthurOW <126161571+ArthurOW@users.noreply.github.com>
2024-05-03 10:02:31 -0300

    arrumei o da aula passada

--- main.js
+++ main.js
@@ -5,12 +5,7 @@ const tempoObjetivo01 = new Date("2024-12-31T00:00:00");
 const tempoObjetivo02 = new Date("2024-12-31T00:00:00");
 const tempoObjetivo03 = new Date("2024-12-31T00:00:00");
 const tempoObjetivo04 = new Date("2024-12-31T00:00:00");
-let tempoAtual = new Date();
 const tempos =[tempoObjetivo01,tempoObjetivo02,tempoObjetivo03,tempoObjetivo04];
-for (let i = 0; i< contadores.length; i++){
-    contadores[i].textContent = calculaTempo(tempos[i]);
-
-}
 for(let i= 0; i< botoes.length; i++){
     botoes[i].onclick = function(){
         for (let j=0; j< botoes.length; j++){
